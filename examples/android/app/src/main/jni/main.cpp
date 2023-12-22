xlin318/SFML

Rewrite `sf::Event` API to improve type safety
This new API is built on top of std::variant. This allows us to
store many different event types in a space-efficient way and access
the active event type in a type-safe manner that eliminates the
categories of UB that are possible with unions.

Co-authored-by: kimci86 <kimci86@hotmail.fr>

examples/android/app/src/main/jni/main.cpp
@@ -115,44 +115,50 @@ int main(int argc, char* argv[])
     {
         for (sf::Event event; active ? window.pollEvent(event) : window.waitEvent(event);)
         {
-            switch (event.type)
+            switch (event.getType())
             {
-                case sf::Event::Closed:
+                case sf::Event::Type::Closed:
                     window.close();
                     break;
-                case sf::Event::KeyPressed:
-                    if (event.key.code == sf::Keyboard::Escape)
+                case sf::Event::Type::KeyPressed:
+                    if (event.get<sf::Event::KeyPressed>().code == sf::Keyboard::Escape)
                         window.close();
                     break;
-                case sf::Event::Resized:
-                    view.setSize(sf::Vector2f(event.size.width, event.size.height));
-                    view.setCenter(sf::Vector2f(event.size.width, event.size.height) / 2.f);
+                case sf::Event::Type::Resized:
+                {
+                    const auto size = sf::Vector2f(event.get<sf::Event::Resized>().size);
+                    view.setSize(size);
+                    view.setCenter(size / 2.f);
                     window.setView(view);
                     break;
-                case sf::Event::LostFocus:
+                }
+                case sf::Event::Type::LostFocus:
                     background = sf::Color::Black;
                     break;
-                case sf::Event::GainedFocus:
+                case sf::Event::Type::GainedFocus:
                     background = sf::Color::White;
                     break;
 
                 // On Android MouseLeft/MouseEntered are (for now) triggered,
                 // whenever the app loses or gains focus.
-                case sf::Event::MouseLeft:
+                case sf::Event::Type::MouseLeft:
                     active = false;
                     break;
-                case sf::Event::MouseEntered:
+                case sf::Event::Type::MouseEntered:
                     active = true;
                     break;
-                case sf::Event::TouchBegan:
-                    if (event.touch.finger == 0)
+                case sf::Event::Type::TouchBegan:
+                {
+                    const auto& touchBegan = event.get<sf::Event::TouchBegan>();
+                    if (touchBegan.finger == 0)
                     {
-                        image.setPosition({static_cast<float>(event.touch.x), static_cast<float>(event.touch.y)});
+                        image.setPosition(sf::Vector2f(touchBegan.position));
 #if defined(USE_JNI)
                         vibrate(sf::milliseconds(10));
 #endif
                     }
                     break;
+                }
             }
         }
 

src/SFML/Window/WindowImpl.cpp
@@ -234,8 +234,10 @@ void WindowImpl::processJoystickEvents()
         if (previousState.connected ^ connected)
         {
             Event event;
-            event.type                      = connected ? Event::JoystickConnected : Event::JoystickDisconnected;
-            event.joystickButton.joystickId = i;
+            if (connected)
+                event = Event::JoystickConnected{i};
+            else
+                event = Event::JoystickDisconnected{i};
             pushEvent(event);
 
             // Clear previous axes positions
@@ -257,13 +259,7 @@ void WindowImpl::processJoystickEvents()
                     const float currPos = m_joystickStatesImpl->states[i].axes[axis];
                     if (std::abs(currPos - prevPos) >= m_joystickThreshold)
                     {
-                        Event event;
-                        event.type                    = Event::JoystickMoved;
-                        event.joystickMove.joystickId = i;
-                        event.joystickMove.axis       = axis;
-                        event.joystickMove.position   = currPos;
-                        pushEvent(event);
-
+                        pushEvent(Event::JoystickMoved{i, axis, currPos});
                         m_previousAxes[i][axis] = currPos;
                     }
                 }
@@ -278,9 +274,10 @@ void WindowImpl::processJoystickEvents()
                 if (prevPressed ^ currPressed)
                 {
                     Event event;
-                    event.type = currPressed ? Event::JoystickButtonPressed : Event::JoystickButtonReleased;
-                    event.joystickButton.joystickId = i;
-                    event.joystickButton.button     = j;
+                    if (currPressed)
+                        event = Event::JoystickButtonPressed{i, j};
+                    else
+                        event = Event::JoystickButtonReleased{i, j};
                     pushEvent(event);
                 }
             }
@@ -308,15 +305,7 @@ void WindowImpl::processSensorEvents()
 
             // If the value has changed, trigger an event
             if (m_sensorValue[i] != previousValue) // @todo use a threshold?
-            {
-                Event event;
-                event.type        = Event::SensorChanged;
-                event.sensor.type = sensor;
-                event.sensor.x    = m_sensorValue[i].x;
-                event.sensor.y    = m_sensorValue[i].y;
-                event.sensor.z    = m_sensorValue[i].z;
-                pushEvent(event);
-            }
+                pushEvent(Event::SensorChanged{sensor, m_sensorValue[i]});
         }
     }
 }

src/SFML/Window/iOS/WindowImplUIKit.mm
@@ -226,9 +226,9 @@ bool WindowImplUIKit::hasFocus() const
 ////////////////////////////////////////////////////////////
 void WindowImplUIKit::forwardEvent(Event event)
 {
-    if (event.type == Event::GainedFocus)
+    if (event.is<Event::GainedFocus>())
         m_hasFocus = true;
-    else if (event.type == Event::LostFocus)
+    else if (event.is<Event::LostFocus>())
         m_hasFocus = false;
 
     pushEvent(event);

Lagermeister/Fork-SFML

examples/android/app/src/main/jni/main.cpp
@@ -115,44 +115,50 @@ int main(int argc, char* argv[])
     {
         for (sf::Event event; active ? window.pollEvent(event) : window.waitEvent(event);)
         {
-            switch (event.type)
+            switch (event.getType())
             {
-                case sf::Event::Closed:
+                case sf::Event::Type::Closed:
                     window.close();
                     break;
-                case sf::Event::KeyPressed:
-                    if (event.key.code == sf::Keyboard::Key::Escape)
+                case sf::Event::Type::KeyPressed:
+                    if (event.get<sf::Event::KeyPressed>().code == sf::Keyboard::Key::Escape)
                         window.close();
                     break;
-                case sf::Event::Resized:
-                    view.setSize(sf::Vector2f(event.size.width, event.size.height));
-                    view.setCenter(sf::Vector2f(event.size.width, event.size.height) / 2.f);
+                case sf::Event::Type::Resized:
+                {
+                    const auto size = sf::Vector2f(event.get<sf::Event::Resized>().size);
+                    view.setSize(size);
+                    view.setCenter(size / 2.f);
                     window.setView(view);
                     break;
-                case sf::Event::LostFocus:
+                }
+                case sf::Event::Type::FocusLost:
                     background = sf::Color::Black;
                     break;
-                case sf::Event::GainedFocus:
+                case sf::Event::Type::FocusGained:
                     background = sf::Color::White;
                     break;
 
                 // On Android MouseLeft/MouseEntered are (for now) triggered,
                 // whenever the app loses or gains focus.
-                case sf::Event::MouseLeft:
+                case sf::Event::Type::MouseLeft:
                     active = false;
                     break;
-                case sf::Event::MouseEntered:
+                case sf::Event::Type::MouseEntered:
                     active = true;
                     break;
-                case sf::Event::TouchBegan:
-                    if (event.touch.finger == 0)
+                case sf::Event::Type::TouchBegan:
+                {
+                    const auto& touchBegan = event.get<sf::Event::TouchBegan>();
+                    if (touchBegan.finger == 0)
                     {
-                        image.setPosition({static_cast<float>(event.touch.x), static_cast<float>(event.touch.y)});
+                        image.setPosition(sf::Vector2f(touchBegan.position));
 #if defined(USE_JNI)
                         vibrate(sf::milliseconds(10));
 #endif
                     }
                     break;
+                }
                 default:
                     break;
             }

src/SFML/Window/WindowImpl.cpp
@@ -233,8 +233,10 @@ void WindowImpl::processJoystickEvents()
         if (previousState.connected ^ connected)
         {
             Event event;
-            event.type                      = connected ? Event::JoystickConnected : Event::JoystickDisconnected;
-            event.joystickButton.joystickId = i;
+            if (connected)
+                event = Event::JoystickConnected{i};
+            else
+                event = Event::JoystickDisconnected{i};
             pushEvent(event);
 
             // Clear previous axes positions
@@ -256,13 +258,7 @@ void WindowImpl::processJoystickEvents()
                     const float currPos = m_joystickStatesImpl->states[i].axes[axis];
                     if (std::abs(currPos - prevPos) >= m_joystickThreshold)
                     {
-                        Event event;
-                        event.type                    = Event::JoystickMoved;
-                        event.joystickMove.joystickId = i;
-                        event.joystickMove.axis       = axis;
-                        event.joystickMove.position   = currPos;
-                        pushEvent(event);
-
+                        pushEvent(Event::JoystickMoved{i, axis, currPos});
                         m_previousAxes[i][axis] = currPos;
                     }
                 }
@@ -277,9 +273,10 @@ void WindowImpl::processJoystickEvents()
                 if (prevPressed ^ currPressed)
                 {
                     Event event;
-                    event.type = currPressed ? Event::JoystickButtonPressed : Event::JoystickButtonReleased;
-                    event.joystickButton.joystickId = i;
-                    event.joystickButton.button     = j;
+                    if (currPressed)
+                        event = Event::JoystickButtonPressed{i, j};
+                    else
+                        event = Event::JoystickButtonReleased{i, j};
                     pushEvent(event);
                 }
             }
@@ -307,15 +304,7 @@ void WindowImpl::processSensorEvents()
 
             // If the value has changed, trigger an event
             if (m_sensorValue[sensor] != previousValue) // TODO use a threshold?
-            {
-                Event event;
-                event.type        = Event::SensorChanged;
-                event.sensor.type = sensor;
-                event.sensor.x    = m_sensorValue[sensor].x;
-                event.sensor.y    = m_sensorValue[sensor].y;
-                event.sensor.z    = m_sensorValue[sensor].z;
-                pushEvent(event);
-            }
+                pushEvent(Event::SensorChanged{sensor, m_sensorValue[sensor]});
         }
     }
 }

src/SFML/Window/iOS/WindowImplUIKit.mm
@@ -226,9 +226,9 @@ bool WindowImplUIKit::hasFocus() const
 ////////////////////////////////////////////////////////////
 void WindowImplUIKit::forwardEvent(Event event)
 {
-    if (event.type == Event::GainedFocus)
+    if (event.is<Event::FocusGained>())
         m_hasFocus = true;
-    else if (event.type == Event::LostFocus)
+    else if (event.is<Event::FocusLost>())
         m_hasFocus = false;
 
     pushEvent(event);